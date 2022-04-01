714aa58248cc21b676bb35b7874833b4936821f4
Vaughn Kromrey <vaughn.kromrey@gmail.com>
2022-03-31 22:32:50 -0500

    Sign Up and Contact Us created, minor changes

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+function contactOn() {
+    document.getElementById("contact").style.display = "block";
+}
+  
+function contactOff() {
+    document.getElementById("contact").style.display = "none";
+}
---
+++ js/signup.js
@@ -0,0 +1,7 @@
+function signUpOn() {
+    document.getElementById("sign_up").style.display = "block";
+}
+  
+function signUpOff() {
+    document.getElementById("sign_up").style.display = "none";
+}
